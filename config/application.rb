08842db12067987045d0ffc2df5a54431adb85c5
Михаил Кашинин <flyaway400@gmail.com>
2019-07-03 01:39:05 +0800

    locales

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,17 @@ module TestGuru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = 'Irkutsk'
+    # Автозагрузка всех языковых файлов
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
 
+    # Дефолтовый язык
+    config.i18n.default_locale = :ru
+
+    # Возврат к английскому при ошибках
+    config.i18n.fallbacks = [:en]
+
+    # Так же можно указывать какой язык использовать при ошибках
+    config.i18n.fallbacks = {'ru' => 'en'}
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
