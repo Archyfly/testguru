c8b663d9c7248d98a88dd1a33707fbc55445c34e
Михаил Кашинин <flyaway400@gmail.com>
2019-07-18 20:37:38 +0800

    Correction instance method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
   def tests_of_user_from_level(testlevel)
-    Test.joins('join user_tests on user_tests.test_id = tests.id').where(user_tests: {user_id: user.id}, level: testlevel)
+    Test.joins('join user_tests on user_tests.user_id = user.id').where(user_tests: {level: testlevel)
   end
 end
