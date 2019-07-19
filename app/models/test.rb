f254b991c923eef57d9b6db07fc765e5c591277d
Михаил Кашинин <flyaway400@gmail.com>
2019-07-20 02:01:44 +0800

    associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,13 @@
 class Test < ApplicationRecord
-belongs_to :category
+  belongs_to :category
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through :tests_users # through - опция, указывающая через какую таблица идет связь
+
+
+  def self.catsort2(catname2)
+    Test.joins(:category).where(categories: {title: catname2}).order('tests.title DESC').pluck(:title)
+  end
 
   # Через 2 запроса (примерно)
   #def self.cat_sort(cat_name)
@@ -14,8 +22,4 @@ belongs_to :category
   #end
 
   # Через 1 запрос
-  def self.catsort2(catname2)
-    Test.joins(:category).where(categories: {title: catname2}).order('tests.title DESC').pluck(:title)
-  end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+has_many :tests_users
+has_many :tests, through :tests_users # through - опция, указывающая через какую таблица идет связь
+
 
   def tests_of_user_from_level(testlevel)
-    Test.joins('join user_tests on user_tests.user_id = user.id').where(user_tests: {level: testlevel)
+    Test.joins('join user_tests on user_tests.user_id = user.id').where(user_tests: {level: testlevel})
   end
 end
