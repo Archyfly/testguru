72d9e463169ba82ba7d00315d5a63c9e61f54576
Михаил Кашинин <flyaway400@gmail.com>
2019-07-22 00:31:07 +0800

    add association author_id to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 has_many :tests_users
 has_many :tests, through: :tests_users # through - опция, указывающая через какую таблица идет связь
-has_many :tests
+has_many :tests, :foreign_key => "author_id"
 
   def tests_of_user_from_level(testlevel)
     Test.joins('join user_tests on user_tests.user_id = user.id').where(user_tests: {level: testlevel})
