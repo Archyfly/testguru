d5f6544530dd7d8a637a5ffda6bf639bd2afa06b
Михаил Кашинин <flyaway400@gmail.com>
2019-10-25 01:13:33 +0800

    add pbar js

--- app/assets/javascripts/utilities/form_inline.js
+++ app/assets/javascripts/utilities/form_inline.js
@@ -1,16 +1,17 @@
 document.addEventListener('turbolinks:load', function() {
-  var controls = document.querySelectorAll('.form-inline-link')
+  $('.form-inline-link').on('click', formInlineLinkHandler) // через jquery
 
-  if (controls.length) {
-  for (var i = 0; i < controls.length; i++) {
-    controls[i].addEventListener('click', formInlineLinkHandler)
-    }
-  }
+//  var controls = document.querySelectorAll('.form-inline-link')
 
-  // $('.form-inline-link').on('click', formInlineHandler) - через jquery
+//  if (controls.length) {
+//  for (var i = 0; i < controls.length; i++) {
+//    controls[i].addEventListener('click', formInlineLinkHandler)
+//    }
+//  }
+
   var errors = document.querySelector('.resource-errors')
-  console.log(errors.innerHTML + ' is ERROR')
-  if (errors.innerHTML) {
+//  console.log(errors.innerHTML + ' is ERROR')
+  if (errors) {
     var resourceId = this.dataset.resourceId
     formInlineHandler(resourceId)
   }
@@ -25,16 +26,19 @@ function formInlineLinkHandler(event) {
 
 function formInlineHandler(testId) {
   var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
-  var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
-  var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
+  // var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
+  // var formInline = document.querySelector('.form-inline[data-test-id="' + testId + '"]')
 
-  if (formInline.classList.contains('hide')) {
-    testTitle.classList.add('hide')
-    formInline.classList.remove('hide')
+  var $testTitle = $('.test-title[data-test-id="' + testId + '"]')
+  var $formInline = $('.form-inline[data-test-id="' + testId + '"]')
+
+  // if ($formInline.classList.contains('hide'))
+  $formInline.toggle()
+  $testTitle.toggle() // эта функция jquery автоматически включает и выключает видимость
+
+  if ($formInline.is(':visible')) {
     link.textContent = 'Cancel'
   } else {
-    testTitle.classList.remove('hide')
-    formInline.classList.add('hide')
     link.textContent = 'Editt'
   }
 }
