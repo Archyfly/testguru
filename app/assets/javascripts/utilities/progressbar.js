c4a479e17acc0124d5cacae7395c9e17299b8357
Михаил Кашинин <flyaway400@gmail.com>
2019-10-25 23:28:21 +0800

    try progress 1

--- app/assets/javascripts/utilities/progressbar.js
+++ app/assets/javascripts/utilities/progressbar.js
@@ -1,47 +1,16 @@
-//document.addEventListener('turbolinks:load', function() {
 
-  // var rec = document.getElementById("example"),
-  //  ctx = rec.getContext('2d');
-  //  ctx.fillRect(0, 0, rec.width, rec.height);
-
-  //var questionCount = document.getElementById('questionCount').innerHTML
-  //console.log(questionCount)
-
-  // for (var i = 0; i < questionCount; i++) {
-    // console.log(i + " question");
-  //  if (rec = document.getElementById(i)) {
-  //    rec = document.getElementById(i),
-  //    ctx = rec.getContext('2d');
-  //    ctx.fillRect(0, 0 + i * 10, rec.width + i*20, rec.height);
-  //  }
-  // }
-// })
 document.addEventListener('turbolinks:load', function() {
   var countOfQuestions = document.querySelector(".countOfQuestions").innerHTML
   var currentQuestionNumber = document.querySelector(".currentQuestionNumber").innerHTML;
 
-  console.log(countOfQuestions)
-  console.log(currentQuestionNumber)
-
-  //<% i = @test_passage.current_question.id %>
-
   var j = 0;
-  console.log(j)
 
   while (j < currentQuestionNumber)  {
     var canvas = document.getElementById("canvas1")
-//      var recStroke = document.getElementById("<%= j %>"),
         ctx = canvas.getContext('2d');
   			document.getElementById("canvas1").width = 30+ j*30;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-
-//      var recFill = document.getElementById("<%= currentQuestionNumber-1 %>"),
-//        ctx = recFill.getContext('2d');
-//  			ctx.fillRect(0, 0, recFill.width, recFill.height);
+        ctx.fillStyle = "green";
+        ctx.fillRect(0, 5, canvas.width, canvas.height);
         j = j + 1;
-  console.log(j)
-  console.log(j-currentQuestionNumber)
-}
-
+  }
 })
