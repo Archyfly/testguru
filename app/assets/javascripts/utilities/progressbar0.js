fea607f17a8ab659d0c8f924d6cff4ef61978650
Михаил Кашинин <flyaway400@gmail.com>
2019-10-31 01:00:32 +0800

    fixed progress bar by comments Andrey, adding correct questions number.

--- app/assets/javascripts/utilities/progressbar0.js
+++ app/assets/javascripts/utilities/progressbar0.js
@@ -8,12 +8,15 @@ document.addEventListener('turbolinks:load', function() {
     console.log("questionIndex is " + progressCurrent)
     console.log("count is " + progressSize)
 
+
+
     progressBar(progressSize, progressCurrent)
   }
 })
 
+
 function progressBar(progressSize, progressCurrent) {
-var j = 0;
+  var j = 0;
   while (j < progressCurrent)  {
     var canvas = document.getElementById("canvas2")
     ctx = canvas.getContext('2d');
@@ -23,18 +26,3 @@ var j = 0;
     j = j + 1;
   }
 }
-
-  // var countOfQuestions = document.querySelector(".countOfQuestions").innerHTML
-  // var currentQuestionNumber = document.querySelector(".currentQuestionNumber").innerHTML;
-
-  // var j = 0;
-
-  // while (j < currentQuestionNumber)  {
-  //   var canvas = document.getElementById("canvas1")
-  //      ctx = canvas.getContext('2d');
-  //			document.getElementById("canvas1").width = 30+ j*30;
-  //      ctx.fillStyle = "green";
-  //      ctx.fillRect(0, 5, canvas.width, canvas.height);
-  //      j = j + 1;
-  //}
-//})
