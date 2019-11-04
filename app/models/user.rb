0a5a5c4239bffcb624cca4a25852720e18786568
Михаил Кашинин <flyaway400@gmail.com>
2019-11-04 23:30:23 +0800

    add destroy dependent to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
   # include Auth - удаляем после подключения гема bcrypt
 
-  has_many :test_passages
+  has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages # through - опция, указывающая через какую таблица идет связь
   has_many :authored_tests, class_name: "Test", foreign_key: :author_id
 
