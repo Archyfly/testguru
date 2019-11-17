e1fd69ede222203bd7841b2b09e43ef61a5f5f53
Михаил Кашинин <flyaway400@gmail.com>
2019-11-18 02:31:58 +0800

    add badge rules, fix views

--- app/services/award_service.rb
+++ app/services/award_service.rb
@@ -2,8 +2,7 @@ class AwardService
 
   def award_user
     Badge.all do |badge|
-      badgerule = badge.rule
-      current_user.user_badges << badge.id if send("#{badgerule.to_a}_award?", badge)
+      current_user.user_badges << badge.id if send("#{badge.rule}_award?", badge)
       current_user.save
     end
   end
@@ -15,7 +14,7 @@ class AwardService
     success_test_passages_count = TestPassage.all.where(is_finished: true, test_id: db_tests.ids, user_id: user.id)
     badge = Badge.find(1)
     return badge if success_test_passages_count >= db_tests_count
-    c
+    
   end
 
   def framework_tests_award?
