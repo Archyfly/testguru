09a63d37f741c81cd5fc0e7cf3897e3c183c1f4d
Михаил Кашинин <flyaway400@gmail.com>
2019-11-19 23:18:59 +0800

    add category databases badge award

--- app/services/award_service.rb
+++ app/services/award_service.rb
@@ -1,27 +1,23 @@
 class AwardService
 
-  def initialize
+  def initialize(user)
     @badges = Badge.all
+    @user = user
   end
 
   def award_user(user)
     @badges.each do |badge|
-      user.user_badges << badge.id if send("#{badge.rule}_award?", badge)
-      user.save!
+      @user.user_badges << badge.id if send("#{badge.rule}_award?", badge)
+      @user.save!
     end
-
   end
 
   def databases_tests_award?(badge)
-
-   return true
-
-#    db_tests = Test.all.where(category_id: 2)
-#    db_tests_count = db_tests_count
-#    user = current_user
-#    success_test_passages_count = TestPassage.all.where(is_finished: true, test_id: db_tests.ids, user_id: user.id)
-#    badge = Badge.find(1)
-#    return badge if success_test_passages_count >= db_tests_count
+    db_tests = Test.all.where(category_id: 2)
+    db_tests_count = db_tests.count
+    success_test_passages_count = TestPassage.all.where(is_finished: true, test_id: db_tests.ids, user_id: @user.id).count
+    badge = Badge.find(1)
+    return true if success_test_passages_count >= db_tests_count
   end
 
   def framework_tests_award?(badge)
