743ee5c2c99f3ad250f12b0e88d6e3b8b3ca1b0a
Михаил Кашинин <flyaway400@gmail.com>
2019-12-10 00:13:55 +0800

    add timer script to timer.html.erb

--- app/assets/javascripts/utilities/testTimer.js
+++ app/assets/javascripts/utilities/testTimer.js
@@ -1,38 +1,38 @@
-document.addEventListener('turbolinks:load', function() {
-  var testDuration = document.querySelector('.setTestDuration').innerHTML
+//document.addEventListener('turbolinks:load', function() {
+  //var testDuration = document.querySelector('.setTestDuration').innerHTML
   //var testStarted = document.querySelector('.timeStartTest').innerHTML
   //var testEnded = testStarted + testDuration*60000
-  console.log(testDuration)
-    
+//  console.log(testDuration)
 
 
-    console.log(minutes + ":" + seconds)
-    console.log(seconds)
-    return {
-      'minutes': minutes,
-      'seconds': seconds
-    }
+
+//    console.log(minutes + ":" + seconds)
+//    console.log(seconds)
+//    return {
+//      'minutes': minutes,
+//      'seconds': seconds
+//    }
   // var timeNow = document.querySelector('.timeNow')
-  if (testDuration) {
-  console.log(testDuration)
-  let duration = testDuration*60000;
-   console.log(duration)
+//  if (testDuration) {
+//  console.log(testDuration)
+//  let duration = testDuration*60000;
+//   console.log(duration)
    //let timerId = setInterval(() => alert('tick'), 2000);
 
-  if (duration) {
+//  if (duration) {
     //setTimeout(sayHi, duration);
-    setInterval(sayHi, 1000);
-  }
-}
-})
+//    setInterval(sayHi, 1000);
+//  }
+//}
+//})
 
 
-function sayHi() {
-  return {
-    'minutes': minutes,
-    'seconds': seconds
-  }
-  seconds = duration - 1000
-  minutes = duration - 1000
-
-}
+// function sayHi() {
+//  return {
+//    'minutes': minutes,
+//    'seconds': seconds
+//  }
+//  seconds = duration - 1000
+//  minutes = duration - 1000
+//
+//}
